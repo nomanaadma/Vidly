5aba7c3a2b6086879e7eaf291538addbed186503
Noman Shoukat <nomanaadma@gmail.com>
2020-08-04 19:35:20 +0500

    Pagination

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,35 +1,40 @@
-import React, { Component } from 'react';
-import { getMovies  } from '../services/fakeMovieService'
-import Like from './common/like';
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
+import Pagination from "./common/pagination";
+import { paginate } from "../utils/paginate";
 
 class Movies extends Component {
-    state = { 
-        movies: getMovies()
-    }
+    state = {
+        movies: getMovies(),
+        pageSize: 4,
+        currentPage: 1,
+    };
 
-    handleDelete = movie => {
-        
-        const movies = this.state.movies.filter(m => m._id !== movie._id);
+    handleDelete = (movie) => {
+        const movies = this.state.movies.filter((m) => m._id !== movie._id);
         this.setState({ movies });
+    };
 
-    }
-
-    handleLike = movie => {
-        
+    handleLike = (movie) => {
         const movies = [...this.state.movies];
-        const index = movies.indexOf((movie));
-        movies[index] = {...movie}
+        const index = movies.indexOf(movie);
+        movies[index] = { ...movie };
         movies[index].liked = !movies[index].liked;
         this.setState({ movies });
+    };
 
-    }
+    handlePageChange = (page) => {
+        this.setState({ currentPage : page });
+    };
 
-    render() { 
-
+    render() {
         const { length: count } = this.state.movies;
+        const { pageSize, currentPage, movies: allMovies } = this.state;
 
-        if(count === 0)
-            return <p>There are no movies in the database.</p>;
+        if (count === 0) return <p>There are no movies in the database.</p>;
+
+        const movies = paginate(allMovies, currentPage, pageSize);
 
         return (
             <React.Fragment>
@@ -46,21 +51,43 @@ class Movies extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {  this.state.movies.map(movie => (
+                        {movies.map((movie) => (
                             <tr key={movie._id}>
                                 <td>{movie.title}</td>
                                 <td>{movie.genre.name}</td>
                                 <td>{movie.numberInStock}</td>
                                 <td>{movie.dailyRentalRate}</td>
-                                <td> { <Like liked={movie.liked} onLike={ () => this.handleLike(movie) } /> } </td>
-                                <td><button onClick={ () => this.handleDelete(movie)} className="btn btn-danger btn-small">Delete</button></td>
+                                <td>
+                                    {
+                                        <Like
+                                            liked={movie.liked}
+                                            onLike={() =>
+                                                this.handleLike(movie)
+                                            }
+                                        />
+                                    }
+                                </td>
+                                <td>
+                                    <button
+                                        onClick={() => this.handleDelete(movie)}
+                                        className="btn btn-danger btn-small"
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
+                <Pagination
+                    itemsCount={count}
+                    pageSize={pageSize}
+                    currentPage={currentPage}
+                    onPageChange={this.handlePageChange}
+                />
             </React.Fragment>
-        )
+        );
     }
 }
- 
+
 export default Movies;
