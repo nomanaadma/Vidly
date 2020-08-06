91a053baa82a3249be601d0a2b4aebf1d1a233d1
Noman Shoukat <nomanaadma@gmail.com>
2020-08-07 02:21:07 +0500

    Extracting Main Table

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSortUp, faSortDown } from '@fortawesome/free-solid-svg-icons';
 
 class TableHeader extends Component {
     raiseSort = path => {
@@ -14,13 +16,19 @@ class TableHeader extends Component {
         this.props.onSort(sortColumn);
     };
 
+    renderSortIcon = column => {
+        if(column.path !== this.props.sortColumn.path) return null;
+        const sortIcon = this.props.sortColumn.order === 'asc' ? faSortUp : faSortDown;
+        return <FontAwesomeIcon icon={sortIcon} />
+    }
+
     render() {
         return (
             <thead>
                 <tr>
                     {this.props.columns.map(column => (
-                        <th key={column.path || column.key} onClick={() => this.raiseSort(column.path)}>
-                            {column.label}
+                        <th className="clickable" key={column.path || column.key} onClick={() => this.raiseSort(column.path)}>
+                            {column.label} {this.renderSortIcon(column)}
                         </th>
                     ))}
                 </tr>
