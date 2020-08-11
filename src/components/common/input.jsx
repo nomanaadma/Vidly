2e73c87fde302f91c31a821849eff8e30915ab3d
Noman Shoukat <nomanaadma@gmail.com>
2020-08-12 01:14:12 +0500

    basic validation in login

---
+++ src/components/common/input.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Input = ({ name, label, value, onChange, error }) => {
+    return (
+        <div className="form-group">
+            <label htmlFor={name}>{label}</label>
+            <input
+                type="text"
+                className="form-control"
+                id={name}
+                name={name}
+                value={value}
+                onChange={onChange}
+            />
+            { error && <div className="alert alert-danger">{error}</div> }
+        </div>
+    );
+};
+
+export default Input;
